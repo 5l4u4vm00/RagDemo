3c6d8aede5f89776743f0e749cea557405eda25a
Aries <ariesaries951@gmail.com>
2025-08-11 21:53:38 +0800

    feat: Modify the docker file to use pip control package.

--- RagDemo_Server/main.py
+++ RagDemo_Server/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 from Routers.chatRouter import router as chatRouter
 
-app = FastAPI(title="我的智能助理", version="v1")
+app = FastAPI(title="RagTool", version="v1")
 app.include_router(chatRouter, tags=["chatBot"])
 
 
-# if __name__ == "__main__":
-#     import uvicorn
-#
-#     uvicorn.run("main:app", host="0.0.0.0", port=8888, reload=True)
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("main:app", host="0.0.0.0", port=8888, reload=True)
